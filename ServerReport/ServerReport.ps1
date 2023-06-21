8750a18b72f431daa33240a5637a2d86d8716669
SavantKevin <kevin.klucsarits@savantcts.com>
2023-06-21 16:05:29 -0400

    more changes 06232023

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -4,3 +4,9 @@ $Name = $env:COMPUTERNAME
 # operating system version
 $Version = (Get-ComputerInfo | select-object OsVersion).OsVersion
 
+# logical disk count
+$DiskCount = (Get-CimInstance CIM_LogicalDisk).count
+
+
+
+
