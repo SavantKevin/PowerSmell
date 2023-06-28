5b193365204c5ffdde03db3f4df9f5053f9db366
SavantKevin <kevin.klucsarits@savantcts.com>
2023-06-28 15:29:51 -0400

    new script to build upon moving forward 06282023

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,25 +1,19 @@
-# retrieve system name
+# retrieve system info
 $Name = $env:COMPUTERNAME
-
-# operating system version
 $Version = (Get-ComputerInfo | select-object OsVersion).OsVersion
-
-# logical disk count
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).count
 
-# some bad examples of output
-# write-host is not good to use
-Write-Host "Server Name: $name"
-
-# some good examples of output
-Write-Output "Computer name: $name"
-Write-Output "Computer Version: $version"
-Write-Output "Computer logical disk count: $diskcount"
-
 # make our own custom object
-[PSCustomObject]@{
-    name = value
+$obj = [PSCustomObject]@{
+    ComputerName = $Name
+    OsVersion = $Version
+    DiskCount = $DiskCount
 }
+
+# show output
+Write-Output $obj
+
+
 
 
 
