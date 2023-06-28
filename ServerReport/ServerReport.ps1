5ec50bd04f47d434366698c6aa02c47b75cea1d7
SavantKevin <kevin.klucsarits@savantcts.com>
2023-06-28 14:48:59 -0400

    new changes 06282023

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -7,6 +7,21 @@ $Version = (Get-ComputerInfo | select-object OsVersion).OsVersion
 # logical disk count
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).count
 
+# some bad examples of output
+# write-host is not good to use
+Write-Host "Server Name: $name"
+
+# some good examples of output
+Write-Output "Computer name: $name"
+Write-Output "Computer Version: $version"
+Write-Output "Computer logical disk count: $diskcount"
+
+# make our own custom object
+[PSCustomObject]@{
+    name = value
+}
+
+
 
 
 
