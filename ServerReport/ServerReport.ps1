e34e4f73099a30c3ac50fd73bf90d32755863c79
SavantKevin <kevin.klucsarits@savantcts.com>
2023-06-28 16:06:59 -0400

    multiple objects in logical disk count

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -2,12 +2,14 @@
 $Name = $env:COMPUTERNAME
 $Version = (Get-ComputerInfo | select-object OsVersion).OsVersion
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).count
+$FreeSpace = (Get-CimInstance CIM_LogicalDisk).FreeSpace
 
 # make our own custom object
 $obj = [PSCustomObject]@{
     ComputerName = $Name
     OsVersion = $Version
     DiskCount = $DiskCount
+    'GB Free' = $FreeSpace
 }
 
 # show output
