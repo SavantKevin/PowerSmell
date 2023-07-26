948c60dd1d531865ea36920c731f8f006a8c9c59
SavantKevin <kevin.klucsarits@savantcts.com>
2023-07-26 14:48:40 -0400

    07262023 CLASS

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,23 +1,26 @@
-# retrieve system info
-$Name = $env:COMPUTERNAME
-$Version = (Get-ComputerInfo | select-object OsVersion).OsVersion
-$DiskCount = (Get-CimInstance CIM_LogicalDisk).count
-$FreeSpace = (Get-CimInstance CIM_LogicalDisk | Where-Object {$_.DeviceID -eq 'C:'}).FreeSpace
+function Get-PowerSmell {
+    # retrieve system info
+    $Name = $env:COMPUTERNAME
+    $Version = (Get-ComputerInfo | select-object OsVersion).OsVersion
+    $DiskCount = (Get-CimInstance CIM_LogicalDisk).count
+    $FreeSpace = (Get-CimInstance CIM_LogicalDisk | Where-Object {$_.DeviceID -eq 'C:'}).FreeSpace
 
-# make our own custom object
-$obj = [PSCustomObject]@{
+    # make our own custom object
+    $obj = [PSCustomObject]@{
     ComputerName = $Name
     OsVersion = $Version
     DiskCount = $DiskCount
     'GB Free' = $FreeSpace / 1Gb
+    }
+
+    # show output
+    Write-Output $obj
+
+    # find the number of temporary files on the C drive and subdirectories
+    $rootPath = "C:\"
+    $tempFilePattern = "*.tmp"
+    $files = Get-ChildItem -Path $rootPath -Filter $tempFilePattern -File -Recurse
+    $fileCount = $files.Count
+    Write-Output "Number of temporary files found: $fileCount"
 }
 
-# show output
-Write-Output $obj
-
-# find the number of temporary files on the C drive and subdirectories
-$rootPath = "C:\"
-$tempFilePattern = "*.tmp"
-$files = Get-ChildItem -Path $rootPath -Filter $tempFilePattern -File -Recurse
-$fileCount = $files.Count
-Write-Output "Number of temporary files found: $fileCount"
